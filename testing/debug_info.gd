be0d9467708c42fb496fc2e905bf1883566b7e7e
jonathan-durbin <jongdurbin@gmail.com>
2026-01-27 01:08:02 -0500

    Add more debug info and some animated static bodies for testing

--- testing/debug_info.gd
+++ testing/debug_info.gd
@@ -1,12 +1,20 @@
 extends Label
 
+
+@export var show_footsteps: bool = true
+
 @onready var player: FirstPersonController3D = $"../Player"
+@onready var left_foot: Panel = $LeftFoot
+@onready var right_foot: Panel = $RightFoot
 
 
 var debug_info: Dictionary[String, String] = {}
 
 func _ready() -> void:
+	left_foot.hide()
+	right_foot.hide()
 	player.jumped.connect(_on_player_jumped)
+	player.in_air.connect(_on_player_in_air)
 	player.landed.connect(_on_player_landed)
 	player.moved.connect(_on_player_moved)
 	player.sprint_state_changed.connect(_on_player_sprint_state_changed)
@@ -15,7 +23,9 @@ func _ready() -> void:
 	
 
 func _physics_process(_delta: float) -> void:
-	var key_sizes: Array[int]
+	if debug_info.size() == 0:
+		return
+	var key_sizes: Array[int] = []
 	key_sizes.assign(debug_info.keys().map(func(x: String): return x.length()))
 	var max_width: int = key_sizes.max() + 2
 	var sorted_keys: Array[String] = debug_info.keys()
@@ -28,14 +38,18 @@ func _physics_process(_delta: float) -> void:
 func _on_player_jumped() -> void:
 	debug_info["player_jumped"] = "TRUE"
 	debug_info["player_landed"] = "FALSE"
+
+func _on_player_in_air() -> void:
+	debug_info["player_in_air"] = "TRUE"
 	
 func _on_player_landed(fall_speed: float) -> void:
 	debug_info["player_jumped"] = "FALSE"
+	debug_info["player_in_air"] = "FALSE"
 	debug_info["player_landed"] = "TRUE"
 	debug_info["fall_speed"] = str(snappedf(fall_speed, 0.001))
 	
-func _on_player_moved(speed_xz: float) -> void:
-	debug_info["player_move_speed"] = str(snappedf(speed_xz, 0.001))
+func _on_player_moved(velocity_xz: Vector3) -> void:
+	debug_info["player_move_speed"] = str(snappedf(velocity_xz.length(), 0.001))
 	
 func _on_player_sprint_state_changed(is_sprinting: bool) -> void:
 	debug_info["is_sprinting"] = str(is_sprinting)
@@ -44,5 +58,17 @@ func _on_player_crouch_state_changed(is_crouching: bool) -> void:
 	debug_info["is_crouching"] = str(is_crouching)
 	
 func _on_player_step_landed(foot: int) -> void:
-	debug_info["player_stepped"] = "LEFT" if foot == 0 else "RIGHT"
+	if foot == 0: # Left
+		debug_info["player_stepped"] = "LEFT"
+		if show_footsteps:
+			left_foot.show()
+			right_foot.hide()
+	else:
+		debug_info["player_stepped"] = "RIGHT"
+		if show_footsteps:
+			left_foot.hide()
+			right_foot.show()
+	if not show_footsteps:
+		left_foot.hide()
+		right_foot.hide()
 	
